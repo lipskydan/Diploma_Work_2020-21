c472ee4015a59d61ee7273ed67fa67cc809210c0
lipskydan <lipsky@knu.ua>
2020-12-25 13:12:17 +0200

    Added app main

--- App/online_equipment_accounting/online_equipment_accounting/app/main/views.py
+++ App/online_equipment_accounting/online_equipment_accounting/app/main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from django.template import RequestContext
+
 
 def main(request):
     return render(request, 'main/main.html')
+
--- App/online_equipment_accounting/online_equipment_accounting/settings.py
+++ App/online_equipment_accounting/online_equipment_accounting/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os, sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'app'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -31,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +58,9 @@ ROOT_URLCONF = 'online_equipment_accounting.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(PROJECT_ROOT, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,7 +82,7 @@ WSGI_APPLICATION = 'online_equipment_accounting.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 
@@ -99,11 +105,43 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CMS_LANGUAGES = {
+## Customize this
+'default': {
+    'public': True,
+    'hide_untranslated': False,
+    'redirect_on_fallback': True,
+},
+1: [
+    {
+        'name': 'Русский',
+        'public': True,
+        'hide_untranslated': False,
+        'redirect_on_fallback': True,
+        'code': 'ru',
+    },
+    {
+        'name': 'Українська',
+        'public': True,
+        'hide_untranslated': False,
+        'redirect_on_fallback': True,
+        'code': 'uk',
+    },
+    {
+        'name': 'English',
+        'public': True,
+        'hide_untranslated': False,
+        'redirect_on_fallback': True,
+        'code': 'en',
+    },
+],
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uk'
 
 TIME_ZONE = 'UTC'
 
