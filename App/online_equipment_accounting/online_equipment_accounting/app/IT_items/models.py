c6496bab006b4484cb8829add0cdc96aeee452cc
lipskydan <lipsky@knu.ua>
2020-12-25 13:16:45 +0200

    Added model PC to models

--- App/online_equipment_accounting/online_equipment_accounting/app/IT_items/models.py
+++ App/online_equipment_accounting/online_equipment_accounting/app/IT_items/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class PC(models.Model):
+    __tablename__ = 'PC'
+
+    inventory_number = models.CharField('інвентарний номер', max_length=200)
+    floor = models.IntegerField('номер поверху')
+    room = models.IntegerField('номер кабінету')
+
+    # pub_date = models.DateTimeField('дата створення')
+
+    name_for_user = 'Персональний комп\'ютор'
+    name = 'PC'
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name + ' - поверх ' + str(self.floor) + ' - кабінет ' + str(self.room) + ' - ' + str(self.inventory_number)
+
+    class Meta:
+        verbose_name = 'Персональний комп\'ютор'
+        verbose_name_plural = 'Персональні комп\'ютори'
