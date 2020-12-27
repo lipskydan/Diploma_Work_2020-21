0d50105b92052f9d23d9715d77af778ba22197c3
lipskydan <lipsky@knu.ua>
2020-12-27 18:32:06 +0200

    Added forms to main

--- App/online_equipment_accounting/online_equipment_accounting/app/main/urls.py
+++ App/online_equipment_accounting/online_equipment_accounting/app/main/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
+from django.conf import settings
+from django.contrib.auth.views import LogoutView
+
+app_name = 'main'
 
 urlpatterns = [
-    path('', views.main, name='main')
+    path('', views.main, name='main'),
+    path('signup', views.user_sign_up, name='user_sign_up'),
+    path('login', views.user_login, name='user_login'),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- App/online_equipment_accounting/online_equipment_accounting/app/main/views.py
+++ App/online_equipment_accounting/online_equipment_accounting/app/main/views.py
@@ -1,8 +1,50 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+# from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 from django.template import RequestContext
+
+from .forms import SignUpForm, LoginForm
 
 
 def main(request):
     return render(request, 'main/main.html')
 
+
+def user_sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    return render(request, 'main/signup.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return render(request, 'main/main.html')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return render(request, 'main/login_invalid.html')
+    else:
+        form = LoginForm()
+    return render(request, 'main/login.html', {'form': form})
+
+
+def user_logout(request):
+    user_logout(request)
