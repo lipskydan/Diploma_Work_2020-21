d6d3dc27c7ba475bd18225260178003e4a0fd47e
lipskydan <lipsky@knu.ua>
2021-02-25 23:00:17 +0200

    work with Chart.js

--- App/online_equipment_accounting/online_equipment_accounting/app/main/views.py
+++ App/online_equipment_accounting/online_equipment_accounting/app/main/views.py
@@ -11,16 +11,11 @@ from django.db import models
 from IT_items.models import Motherboard, PowerSupply, PC, VideoCard, LanCard, SoundCard
 
 
-def main(request):
-    pcs = PC.objects.all()
-
-    #####################
-    motherboards = Motherboard.objects.all()
-
-    labels_motherboard_brand = []
-    data_motherboard_brand = []
+def get_motherboard_brand_data_and_labels():
     motherboard_dict = dict()
     queryset_motherboard = Motherboard.objects.order_by('brand')[:]
+    labels_motherboard_brand = []
+    data_motherboard_brand = []
 
     for qs in queryset_motherboard:
         if qs.brand not in motherboard_dict:
@@ -32,9 +27,11 @@ def main(request):
     for key in motherboard_dict.keys():
         data_motherboard_brand.append(motherboard_dict[key])
 
-    print(motherboard_dict)
-    motherboard_dict = dict()
+    return data_motherboard_brand, labels_motherboard_brand
 
+
+def get_motherboard_form_factor_data_and_labels():
+    motherboard_dict = dict()
     queryset_motherboard = Motherboard.objects.order_by('form_factor')[:]
     labels_motherboard_form_factor = []
     data_motherboard_form_factor = []
@@ -49,15 +46,57 @@ def main(request):
     for key in motherboard_dict.keys():
         data_motherboard_form_factor.append(motherboard_dict[key])
 
-    print(motherboard_dict)
+    return data_motherboard_form_factor, labels_motherboard_form_factor
+
+
+def get_motherboard_integrated_items_data_and_labels():
+    data_motherboard_integrated_graphics = Motherboard.objects.filter(integrated_graphics=True).count()
+    data_motherboard_integrated_sound_card = Motherboard.objects.filter(integrated_sound_card=True).count()
+    data_motherboard_integrated_lan_card = Motherboard.objects.filter(integrated_lan_card=True).count()
+    motherboards_count = Motherboard.objects.all().count()
+
+    data_motherboard = [data_motherboard_integrated_graphics, data_motherboard_integrated_sound_card,
+                        data_motherboard_integrated_lan_card, motherboards_count]
+
+    labels_motherboard = ['integrated graphics',
+                          'integrated sound card',
+                          'integrated_lan_card',
+                          'total']
+
+    return data_motherboard, labels_motherboard
+
+    
+def get_motherboard_ram_data_and_labels():
     motherboard_dict = dict()
+    queryset_motherboard = Motherboard.objects.order_by('form_factor')[:]
+    labels_motherboard_ram = []
+    data_motherboard_ram = []
 
-    #####################
+    for qs in queryset_motherboard:
+        if qs.type_ram_slot not in motherboard_dict:
+            motherboard_dict[qs.type_ram_slot] = 1
+            labels_motherboard_ram.append(qs.type_ram_slot)
+        else:
+            motherboard_dict[qs.type_ram_slot] += 1
 
+    for key in motherboard_dict.keys():
+        data_motherboard_ram.append(motherboard_dict[key])
+
+    return data_motherboard_ram, labels_motherboard_ram
+
+
+def main(request):
+    pcs = PC.objects.all()
+    motherboards = Motherboard.objects.all()
     power_supplies = PowerSupply.objects.all()
     video_cards = VideoCard.objects.all()
     lan_cards = LanCard.objects.all()
     sound_cards = SoundCard.objects.all()
+
+    data_motherboard_brand, labels_motherboard_brand = get_motherboard_brand_data_and_labels()
+    data_motherboard_form_factor, labels_motherboard_form_factor = get_motherboard_form_factor_data_and_labels()
+    data_motherboard_integrated_items, labels_motherboard_integrated_items = get_motherboard_integrated_items_data_and_labels()
+    data_motherboard_ram, labels_motherboard_ram = get_motherboard_ram_data_and_labels()
 
     power_supplies_count = power_supplies.count()
 
@@ -66,6 +105,10 @@ def main(request):
                                               'data_motherboard_brand': data_motherboard_brand,
                                               'labels_motherboard_form_factor': labels_motherboard_form_factor,
                                               'data_motherboard_form_factor': data_motherboard_form_factor,
+                                              'labels_motherboard_integrated_items': labels_motherboard_integrated_items,
+                                              'data_motherboard_integrated_items': data_motherboard_integrated_items,
+                                              'labels_motherboard_ram': labels_motherboard_ram,
+                                              'data_motherboard_ram': data_motherboard_ram,
                                               'pcs': pcs,
                                               'power_supplies': power_supplies,
                                               'power_supplies_count': power_supplies_count,
